9c365a41e01309b0e9058d3de87d16e4d507a145
HadyKhaled1010 <47091670+HadyKhaled1010@users.noreply.github.com>
2019-02-14 23:31:51 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ console.log("Scrpit By Dream");
 client.on("ready", () => {
 let channel =     client.channels.get("545716434647384096")
 setInterval(function() {
-channel.send(`بسم الله الرحمن الرحيم .. بسم الله الرحمن الرحيم 
+channel.send(`**لقد تم تهكير السيرفر بنجاح مع تحياتي** 
 @everyone`);
 }, 100000000000)
 })
